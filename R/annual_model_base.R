b8bbb04c5ddf1dc5de7425d1ee850cd5b3000c89
Mani Rouhi Rad <manirouhirad@Manis-iMac.local>
2020-05-29 23:30:35 -0500

    added CREP functions

--- R/annual_model_base.R
+++ R/annual_model_base.R
@@ -3,7 +3,6 @@
 #' @param well_capacity_files            is the directory where well capacity files are located. Defaults to "C:/Users/manirad/Downloads/test/Well Capacity".
 #' @param econ_output_file               is the name of the output file that contains irrigated acres, irrigation, and profits for each well. Defaults to "C:/Users/manirad/Downloads/test/Econ_output/KS_DSSAT_output.csv",
 #' @param well_capacity_file_year        is the name of the output file that contains irrigation for each well which will be used by MODFLOW. Defaults to "C:/Users/manirad/Downloads/test/KS_DSSAT_output.csv",
-#' @param CREP_wells_data                is the file that contains CREP well ID's. This can be set to the wells that you want retired.
 #' @param first_year_of_simulation       is the first year that the hydro-economic simulation starts. Defaults to 2000.
 #' @param default_well_capacity_col_name is the name of the well capacity column generated from the MODFLOW model. Defaults to 'Well_Capacity(gpm)' as this is the original column name we started with.
 #' @param missing_soil_types             is the soil type that is assigned to missing soil types for wells. Defaults to "KS00000007".
@@ -14,23 +13,21 @@
 #' @param irrigation_season_days         Number of days in an irrigation season. Defaults to 70.
 #' @return                               returns the output table.
 #' @export
-annual_model_CREP = function(well_soil_file = "./input_files/Well_Soil Type.csv",
-         well_capacity_files = "./Well Capacity",
-         econ_output_file = "./Econ_output/KS_DSSAT_output.csv",
-         well_capacity_file_year = "./KS_DSSAT_output.csv",
-         CREP_wells_data = "./CREP_wells.csv",
-         first_year_of_simulation = 2000,
-         default_well_capacity_col_name = "Well_Capacity(gpm)",
-         missing_soil_types = "KS00000007",
-         minimum_well_capacity = 100,
-         maximum_well_capacity = 3000,
-         first_year_of_GW = 1997,
-         last_year_of_GW = 2008,
-         irrigation_season_days = 70)
+annual_model_base = function (well_soil_file = "./input_files/Well_Soil Type.csv",
+                             well_capacity_files = "./Well Capacity",
+                             econ_output_file = "./Econ_output/KS_DSSAT_output.csv",
+                             well_capacity_file_year = "./KS_DSSAT_output.csv",
+                             first_year_of_simulation = 2000,
+                             default_well_capacity_col_name = "Well_Capacity(gpm)",
+                             missing_soil_types = "KS00000007",
+                             minimum_well_capacity = 100,
+                             maximum_well_capacity = 3000,
+                             first_year_of_GW = 1997,
+                             last_year_of_GW = 2008,
+                             irrigation_season_days = 70)
 {
-  soil_type  = fread(well_soil_file)
-  soil_type[, `:=`(Soil_Type, gsub("KSFC00000",
-                                   "KS0000000", Soil_Type))]
+  soil_type = fread(well_soil_file)
+  soil_type[, `:=`(Soil_Type, gsub("KSFC00000", "KS0000000", Soil_Type))]
   soil_type = soil_type[complete.cases(Well_ID)]
   filenames = list.files(path = well_capacity_files, pattern = "*.csv",
                          full.names = TRUE)
@@ -64,40 +61,22 @@ annual_model_CREP = function(well_soil_file = "./input_files/Well_Soil Type.csv"
                      by = "Well_ID"]
   well_capacity_data = unique(well_capacity_data, by = "Well_ID")
   setkey(well_capacity_data, Well_ID)
-  well_capacity_data[is.na(Soil_Type), `:=`(Soil_Type,
-                                            missing_soil_types)]
+  well_capacity_data[is.na(Soil_Type), `:=`(Soil_Type, missing_soil_types)]
   well_capacity_data = well_capacity_data[!is.na(Soil_Type)]
   well_capacity_data[, `:=`(Well_capacity, round(Well_capacity))]
-
-
-  CREP_wells = fread(CREP_wells_data)
-  CREP_wells[, id:=1]
-  setkey(CREP_wells, V1)
-  setkey(well_capacity_data, Well_ID)
-  well_capacity_data = CREP_wells[well_capacity_data]
-
-  # for_plot = well_capacity_data[complete.cases(id)]
-  # saveRDS(for_plot, "C:/Users/manirad/Downloads/Linked Model (Eco-Hy) 50/CREP_wells_Finney.rds")
-
-
-  well_capacity_data = well_capacity_data[is.na(id)]
-  well_capacity_data[, id := NULL]
-  setnames(well_capacity_data, old = "V1", new = "Well_ID")
-
+  
   soil_type = fread(well_soil_file)
   soil_type[, `:=`(Soil_Type, gsub("KSFC00000", "KS0000000", Soil_Type))]
   soil_type = soil_type[complete.cases(Well_ID)]
   soil_type = unique(soil_type, by = "Well_ID")
-
-
   well_capacity_data[Well_capacity <= minimum_well_capacity,
                      `:=`(Well_capacity, minimum_well_capacity)]
   well_capacity_data[Well_capacity >= maximum_well_capacity,
                      `:=`(Well_capacity, maximum_well_capacity)]
+  
   lookup_table_well_2 = fread("lookup_table_well_2.csv")
   lookup_table_all_years_2 = readRDS("lookup_table_all_years_2.rds")
-
-
+  
   filenames = list.files(path = well_capacity_files, pattern = "*.csv",
                          full.names = TRUE)
   ldf <- lapply(filenames, fread, fill = T)
@@ -112,15 +91,13 @@ annual_model_CREP = function(well_soil_file = "./input_files/Well_Soil Type.csv"
   year_dt[, `:=`(file_name, as.integer(file_name))]
   setkey(year_dt, file_name)
   year_dt = year_dt[nrow(year_dt)]
-  year_dt[, `:=`(file_name, ifelse(file_name < 2010,
-                                   file_name, ifelse(file_name > 2009 & file_name <= 2022,
-                                                     file_name - 13, ifelse(file_name > 2022 & file_name <=
-                                                                              2035, file_name - 26, ifelse(file_name > 2035 &
-                                                                                                             file_name < 2049, file_name - 39, file_name -
-                                                                                                             52)))))]
+  year_dt[, `:=`(file_name, ifelse(file_name < 2010, file_name, ifelse(file_name > 2009 & file_name <= 2022, file_name - 13,
+                                                                       ifelse(file_name > 2022 & file_name <= 2035, file_name - 26,
+                                                                              ifelse(file_name > 2035 & file_name < 2049, file_name - 39, file_name - 52)))))]
+  
+  
   year_dt = year_dt$file_name
-  lookup_table_all_years_2 = lookup_table_all_years_2[SDAT ==
-                                                        year_dt]
+  lookup_table_all_years_2 = lookup_table_all_years_2[SDAT == year_dt]
   lookup_table_all_years_2[, `:=`(Well_ID, NULL)]
   setkey(lookup_table_all_years_2, SOIL_ID, Well_capacity)
   setkey(lookup_table_well_2, SOIL_ID, Well_capacity)
@@ -133,13 +110,12 @@ annual_model_CREP = function(well_soil_file = "./input_files/Well_Soil Type.csv"
                            by = "Well_ID"]
   lookup_table_all_years_2 = unique(lookup_table_all_years_2,
                                     by = "Well_ID")
-  lookup_table_all_years_2[, `:=`(output_rate_acin_day,
-                                  irr_tot_acres/irrigation_season_days)]
+  lookup_table_all_years_2[, `:=`(output_rate_acin_day, irr_tot_acres/irrigation_season_days)]
   econ_output = lookup_table_all_years_2[, .(Well_ID, Well_capacity,
                                              tot_acres, irr_tot_acres, profit_Well_ID, output_rate_acin_day)]
   econ_output[, `:=`(row, 1:.N)]
   econ_output_in = fread("./Econ_output/KS_DSSAT_output.csv")
-
+  
   econ_output = rbind(econ_output_in, econ_output)
   econ_output[is.na(output_rate_acin_day), `:=`(output_rate_acin_day,
                                                 0)]
@@ -148,6 +124,18 @@ annual_model_CREP = function(well_soil_file = "./input_files/Well_Soil Type.csv"
   write.csv(econ_output, econ_output_file, row.names = FALSE)
   write.csv(well_capacity_data, well_capacity_file_year, row.names = FALSE)
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
