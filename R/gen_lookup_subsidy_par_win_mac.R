5f1f076e6387cf574ba9f4fb19de391f6dc4218a
Mani Rouhi Rad <manirouhirad@Manis-MBP-2.attlocal.net>
2021-02-23 08:54:08 -0500

    function inside a function problem in snow package for windows

--- R/gen_lookup_subsidy_par_win_mac.R
+++ R/gen_lookup_subsidy_par_win_mac.R
@@ -354,7 +354,7 @@ gen_lookup_subsidy_par_win_mac = function (subsidy_amount = 21,
       library(snow)
       cl <- makeCluster(num_clusters)
       print(Sys.info()[1])
-      clusterExport(cl, varlist = c("foo_irr", "data.table",
+      clusterExport(cl, varlist = c("foo_irr", "data.table", "FN_optim2",
                                     "setnames", "setkey", "subsidy_amount", "subsidy_threshold"),
                     envir = environment())
       foo_dt_all <- parLapply(cl, 1:aa, FN_optim2)
