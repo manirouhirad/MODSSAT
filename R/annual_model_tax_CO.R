4f3619ab7df6187d88dc75e91b0574c54dc688e0
Mani Rouhi Rad <manirouhirad@Manis-MBP-2.attlocal.net>
2021-07-21 00:39:33 -0400

    updated the annual CO tax script.

--- R/annual_model_tax_CO.R
+++ R/annual_model_tax_CO.R
@@ -12,6 +12,7 @@
 #' @param maximum_well_capacity          is the maximum well capacity in the model. If well capacity falls above this capacity, it is set to this maximum. Defaults to 3000 gallons per minute.
 #' @param first_year_of_GW               is the first year that the GW model exists. This may be different than the first year of simulation. Defaults to 1997.
 #' @param last_year_of_GW                is the last  year that the GW model exists. This may be different than the last  year of simulation. Defaults to 2007.
+#' @param well_capacity_intervals        is the intervals in well capacities. Defaults to 20.
 #' @param irrigation_season_days         Number of days in an irrigation season. Defaults to 70.
 #' @return                               returns the output table.
 #' @examples
@@ -19,21 +20,21 @@
 #' gen_lookup_tax(tax_amount = 2)
 #' }
 #' @export
-annual_model_tax_CO = function(tax_amount = 1,
-                            # soil_weather_file = "./input_files/Well_Soil Type.csv",
-                            soil_weather_file = "./input_files/Well_SoilType_WeatherStation.csv",
-                            well_capacity_files = "./Well Capacity",
-                            maximum_pump_rate = "./2000_Well_Capacity.csv", 
-                            econ_output_file = "./Econ_output/CO_DSSAT_output.csv", 
-                            well_capacity_file_year = "./CO_DSSAT_output.csv",
-                            first_year_of_simulation = 2000,
-                            default_well_capacity_col_name = "Well_Capacity(gpm)",
-                            missing_soil_types = "KS00000007",
-                            minimum_well_capacity = 1,
-                            maximum_well_capacity = 1000,
-                            first_year_of_GW = 1997,
-                            last_year_of_GW = 2007,
-                            irrigation_season_days = 70)
+annual_model_tax_CO = function (tax_amount = 1,
+                                soil_weather_file   = "./input_files/Well_SoilType_WeatherStation_CO.csv",
+                                well_capacity_files = "./Well Capacity",
+                                maximum_pump_rate =   "./2000_Well_Capacity.csv",
+                                econ_output_file =    "./Econ_output/CO_DSSAT_output.csv",
+                                well_capacity_file_year = "./CO_DSSAT_output.csv",
+                                first_year_of_simulation = 2000,
+                                default_well_capacity_col_name = "Well_Capacity(gpm)",
+                                missing_soil_types = "KS00000007",
+                                minimum_well_capacity = 1,
+                                maximum_well_capacity = 1000,
+                                first_year_of_GW = 2000,
+                                last_year_of_GW = 2018,
+                                well_capacity_intervals = 20,
+                                irrigation_season_days = 70)
 {
   tax_amount = (tax_amount - 1)/10
   soil_type = fread(soil_weather_file)
@@ -55,8 +56,7 @@ annual_model_tax_CO = function(tax_amount = 1,
   setkey(year_dt, file_name)
   year_dt = year_dt[nrow(year_dt)]
   year_dt = year_dt$file_name
-  year_2  = year_dt
-  
+  year_2 = year_dt
   well_capacity = data.table(Well_ID = NA, V1 = NA)
   setnames(well_capacity, old = "V1", new = default_well_capacity_col_name)
   well_capacity = ifelse(year_dt == first_year_of_simulation,
@@ -76,33 +76,27 @@ annual_model_tax_CO = function(tax_amount = 1,
   setkey(well_capacity_data, Well_ID)
   well_capacity_data[is.na(Soil_Type), `:=`(Soil_Type, missing_soil_types)]
   well_capacity_data = well_capacity_data[!is.na(Soil_Type)]
-  # well_capacity_data[, `:=`(Well_capacity, round(Well_capacity))]
-  well_capacity_data[is.na(weather_station), `:=`(weather_station, well_capacity_data[1,weather_station])]
+  well_capacity_data[is.na(weather_station), `:=`(weather_station,
+                                                  well_capacity_data[1, weather_station])]
   well_capacity_data = well_capacity_data[!is.na(weather_station)]
   well_capacity_data[, `:=`(Well_capacity, round(Well_capacity))]
-  
-  
-  # soil_type = fread(soil_weather_file)
-  # soil_type[, `:=`(Soil_Type, gsub("KSFC00000", "KS0000000",
-  #                                  Soil_Type))]
-  # soil_type = soil_type[complete.cases(Well_ID)]
-  # soil_type = unique(soil_type, by = "Well_ID")
-  
   maximum_pump_rate_dt = fread(maximum_pump_rate)
-  
-  setkey(well_capacity_data,   Well_ID)
+  setkey(well_capacity_data, Well_ID)
   setkey(maximum_pump_rate_dt, Well_ID)
   well_capacity_data = well_capacity_data[maximum_pump_rate_dt]
-  well_capacity_data[, `Well_Capacity(gpm)` := round(`Well_Capacity(gpm)`)]
-  well_capacity_data[Well_capacity > `Well_Capacity(gpm)`, Well_capacity := `Well_Capacity(gpm)`]
-  well_capacity_data[, `Well_Capacity(gpm)` := NULL]
-  
-  well_capacity_data[, well_capacity_org := Well_capacity]
-  well_capacity_data[Well_capacity <= minimum_well_capacity, `:=`(Well_capacity, minimum_well_capacity)]
-  well_capacity_data[Well_capacity >= maximum_well_capacity, `:=`(Well_capacity, maximum_well_capacity)]
-  
+  well_capacity_data[, `:=`(`Well_Capacity(gpm)`, round(`Well_Capacity(gpm)`))]
+  well_capacity_data[Well_capacity > `Well_Capacity(gpm)`,
+                     `:=`(Well_capacity, `Well_Capacity(gpm)`)]
+  well_capacity_data[, `:=`(`Well_Capacity(gpm)`, NULL)]
+  well_capacity_data[, `:=`(well_capacity_org, Well_capacity)]
+  well_capacity_data[Well_capacity <= minimum_well_capacity,
+                     `:=`(Well_capacity, minimum_well_capacity)]
+  well_capacity_data[Well_capacity >= maximum_well_capacity,
+                     `:=`(Well_capacity, maximum_well_capacity)]
+  well_capacity_data[, Well_capacity := floor(Well_capacity/well_capacity_intervals)*well_capacity_intervals]
+
   lookup_table_all_years_2 = readRDS("lookup_table_all_years_2.rds")
-  
+  lookup_table_all_years_2 = unique(lookup_table_all_years_2, by=colnames(lookup_table_all_years_2))
   filenames = list.files(path = well_capacity_files, pattern = "*.csv",
                          full.names = TRUE)
   ldf <- lapply(filenames, fread, fill = T)
@@ -117,64 +111,42 @@ annual_model_tax_CO = function(tax_amount = 1,
   year_dt[, `:=`(file_name, as.integer(file_name))]
   setkey(year_dt, file_name)
   year_dt = year_dt[nrow(year_dt)]
-  # year_dt[, `:=`(file_name, ifelse(file_name < last_year_of_GW +
-  #                                    1, file_name, ifelse(file_name > last_year_of_GW & file_name <=
-  #                                                           last_year_of_GW + (last_year_of_GW - first_year_of_GW +
-  #                                                                                1), file_name - (last_year_of_GW - first_year_of_GW +
-  #                                                                                                   1), ifelse(file_name > last_year_of_GW + (last_year_of_GW -
-  #                                                                                                                                               first_year_of_GW + 1) & file_name <= last_year_of_GW +
-  #                                                                                                                2 * (last_year_of_GW - first_year_of_GW + 1), file_name -
-  #                                                                                                                2 * (last_year_of_GW - first_year_of_GW), ifelse(file_name >
-  #                                                                                                                                                                   last_year_of_GW + 2 * (last_year_of_GW - first_year_of_GW +
-  #                                                                                                                                                                                            1) & file_name < last_year_of_GW + 3 * (last_year_of_GW -
-  #                                                                                                                                                                                                                                      first_year_of_GW + 1), file_name - 3 * (last_year_of_GW -
-  #                                                                                                                                                                                                                                                                                first_year_of_GW), file_name - 4 * (last_year_of_GW -
-  #                                                                                                                                                                                                                                                                                                                      first_year_of_GW))))))]
-  
-  # year_dt[, `:=`(file_name, ifelse(file_name < 2010, file_name, ifelse(file_name > 2009 & file_name <= 2022, file_name - 13,
-  #                                                                      ifelse(file_name > 2022 & file_name <= 2035, file_name - 26,
-  #                                                                             ifelse(file_name > 2035 & file_name < 2049, file_name - 39, file_name - 52)))))]
-  
-  # year_dt[, `:=`(file_name, ifelse(file_name <= 2006, file_name+1,
-  #                                  ifelse(file_name > 2006 & file_name <= 2017, file_name - 10, ifelse(file_name > 2017 & file_name <= 2028,
-  #                                                                                                      file_name - 21, ifelse(file_name > 2028 & file_name <= 2039, file_name - 32, file_name - 43)))))]
-  #
-  # year_dt = year_dt$file_name
-  
-  year_dt[, `:=`(file_name, ifelse(file_name <= (last_year_of_GW-1), file_name +
-                                     1, ifelse(file_name > (last_year_of_GW-1) & file_name <= (last_year_of_GW-1 + last_year_of_GW - first_year_of_GW+1), file_name -
-                                                 (-1 + last_year_of_GW - first_year_of_GW+1), ifelse(file_name > (last_year_of_GW-1 + last_year_of_GW - first_year_of_GW+1) & file_name <= (last_year_of_GW-1 + 2*(last_year_of_GW - first_year_of_GW+1)), file_name -
-                                                                                                       (-1 + 2*(last_year_of_GW - first_year_of_GW+1)), ifelse(file_name > (last_year_of_GW-1 + 2*(last_year_of_GW - first_year_of_GW+1)) & file_name <= (last_year_of_GW-1 + 3*(last_year_of_GW - first_year_of_GW+1)), file_name -
-                                                                                                                                                                 (-1 + 3*(last_year_of_GW - first_year_of_GW+1)), file_name - (-1 + 4*(last_year_of_GW - first_year_of_GW+1)))))))]
+  year_dt[, `:=`(file_name, ifelse(file_name <= (last_year_of_GW -
+                                                   1), file_name + 1, ifelse(file_name > (last_year_of_GW -
+                                                                                            1) & file_name <= (last_year_of_GW - 1 + last_year_of_GW -
+                                                                                                                 first_year_of_GW + 1), file_name - (-1 + last_year_of_GW -
+                                                                                                                                                       first_year_of_GW + 1), ifelse(file_name > (last_year_of_GW -
+                                                                                                                                                                                                    1 + last_year_of_GW - first_year_of_GW + 1) & file_name <=
+                                                                                                                                                                                       (last_year_of_GW - 1 + 2 * (last_year_of_GW - first_year_of_GW +
+                                                                                                                                                                                                                     1)), file_name - (-1 + 2 * (last_year_of_GW - first_year_of_GW +
+                                                                                                                                                                                                                                                   1)), ifelse(file_name > (last_year_of_GW - 1 + 2 * (last_year_of_GW -
+                                                                                                                                                                                                                                                                                                         first_year_of_GW + 1)) & file_name <= (last_year_of_GW -
+                                                                                                                                                                                                                                                                                                                                                  1 + 3 * (last_year_of_GW - first_year_of_GW + 1)), file_name -
+                                                                                                                                                                                                                                                                 (-1 + 3 * (last_year_of_GW - first_year_of_GW + 1)),
+                                                                                                                                                                                                                                                               file_name - (-1 + 4 * (last_year_of_GW - first_year_of_GW +
+                                                                                                                                                                                                                                                                                        1)))))))]
   year_dt = year_dt$file_name
-  
-
-  lookup_table_all_years_2 = lookup_table_all_years_2[SDAT == year_dt]
+  lookup_table_all_years_2 = lookup_table_all_years_2[SDAT ==
+                                                        year_dt]
   lookup_table_all_years_2[, `:=`(Well_ID, NULL)]
-  setkey(lookup_table_all_years_2,   WSTA, SOIL_ID, Well_capacity)
+
+  setkey(lookup_table_all_years_2, WSTA, SOIL_ID, Well_capacity)
   setkey(well_capacity_data, weather_station, Soil_Type, Well_capacity)
+
   lookup_table_all_years_2 = lookup_table_all_years_2[well_capacity_data]
-  
-  # lookup_table_all_years_2[, `:=`(irr_tot_acres, sum(irrigation)), by = "Well_ID"]
-  # lookup_table_all_years_2[, `:=`(profit_Well_ID, sum(profit)),    by = "Well_ID"]
-  # lookup_table_all_years_2 = unique(lookup_table_all_years_2,      by = "Well_ID")
-  
-  # lookup_table_all_years_2[, `:=`(output_rate_acin_day, irr_tot_acres/irrigation_season_days)]
-  econ_output = lookup_table_all_years_2[, .(Well_ID, well_capacity = well_capacity_org, tot_acres, quarter, CR, PAW, irrigation, profit)]
+  lookup_table_all_years_2 = unique(lookup_table_all_years_2, by=c("Well_ID", "quarter"))
+
+  lookup_table_all_years_2 = lookup_table_all_years_2[quarter <= QCircle_Count]
+  lookup_table_all_years_2[, foo := ifelse(CR %like% "dry" | CR == "FA", 0, 32.5)]
+  lookup_table_all_years_2[, tot_acres:= sum(foo), by="Well_ID"]
+
+  econ_output = lookup_table_all_years_2[, .(Well_ID, well_capacity = well_capacity_org,
+                                             tot_acres, quarter, CR, PAW, irrigation_ac_in = irrigation, irrigation_depth_mm = round(irrigation/32.5 * 25.4), profit)]
   econ_output[, `:=`(row, 1:.N)]
   econ_output[, `:=`(tax, tax_amount)]
-  
   write.csv(econ_output, well_capacity_file_year, row.names = FALSE)
-  
-  # CO_DSSAT_output = econ_output[1]
-  # CO_DSSAT_output[, Well_ID := 0]
-  # write.csv(CO_DSSAT_output, "./input_files/CO_DSSAT_output.csv", row.names = F)
-  
+
   econ_output_in = fread(econ_output_file)
   econ_output = rbind(econ_output_in, econ_output)
-  # econ_output[is.na(output_rate_acin_day), `:=`(output_rate_acin_day, 0)]
-  # econ_output[, `:=`(output_rate_acin_day, 0)]
-  # well_capacity_data = lookup_table_all_years_2[, .(Well_ID, output_rate_acin_day=0)]
-  # print(well_capacity_data)
   write.csv(econ_output, econ_output_file, row.names = FALSE)
 }
