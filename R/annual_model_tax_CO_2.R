2167ae30eb4480749bca1ee9ec1f02e49be286de
manirouhirad <manirouhirad@82.162.21.198.tigernet.wifi.dyn.clemson.edu>
2021-09-07 13:01:53 -0400

    fixed the issue with IFREQ not being in the output.

--- R/annual_model_tax_CO_2.R
+++ R/annual_model_tax_CO_2.R
@@ -173,6 +173,10 @@ annual_model_tax_CO_2 = function (tax_amount = 1,
   econ_output[, `:=`(tax, tax_amount)]
   write.csv(econ_output, well_capacity_file_year, row.names = FALSE)
 
+  econ_output = lookup_table_all_years_2[, .(Well_ID, well_capacity = well_capacity_org,
+                                             tot_acres, IFREQ, quarter, CR, PAW, irrigation_ac_in = irrigation, irrigation_depth_mm = round(irrigation/32.5 * 25.4), profit, yield_kg_ac)]
+  econ_output[, `:=`(row, 1:.N)]
+  econ_output[, `:=`(tax, tax_amount)]
   econ_output = econ_output[,.(Well_ID, year = year_dt, well_capacity, tot_acres, IFREQ, quarter, CR, PAW, irrigation = irrigation_ac_in, profit, yield_kg_ac, row, tax)]
   econ_output_in = fread(econ_output_file)
   econ_output = rbind(econ_output_in, econ_output)
