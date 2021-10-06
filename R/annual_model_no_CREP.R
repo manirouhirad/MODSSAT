b154daabdfc157fa448e9e551a39a619d145eb3a
manirouhirad <manirouhirad@Manis-MBP-2.attlocal.net>
2021-10-06 10:41:00 -0400

    updated all functions for CREP. Changes are minor.

--- R/annual_model_no_CREP.R
+++ R/annual_model_no_CREP.R
@@ -3,32 +3,37 @@
 #' @param well_capacity_files            is the directory where well capacity files are located. Defaults to "C:/Users/manirad/Downloads/test/Well Capacity".
 #' @param econ_output_folder             is the name of the output folder that contains irrigated acres, irrigation, and profits for each well. Defaults to "C:/Users/manirad/Downloads/test/Econ_output/KS_DSSAT_output.csv",
 #' @param well_capacity_file_year        is the name of the output file that contains irrigation for each well which will be used by MODFLOW. Defaults to "C:/Users/manirad/Downloads/test/KS_DSSAT_output.csv",
+#' @param look_up_table                  is the lookup table for wells that are inside  the selected policy area.
 #' @param CREP_wells_data                is the file that contains CREP well ID's. This can be set to the wells that you want retired.
 #' @param first_year_of_simulation       is the first year that the hydro-economic simulation starts. Defaults to 2000.
 #' @param default_well_capacity_col_name is the name of the well capacity column generated from the MODFLOW model. Defaults to 'Well_Capacity(gpm)' as this is the original column name we started with.
 #' @param missing_soil_types             is the soil type that is assigned to missing soil types for wells. Defaults to "KS00000007".
 #' @param minimum_well_capacity          is the minimum well capacity in the model. If well capacity falls below this capacity, it is set to this minimum. Defaults to 100  gallons per minute.
 #' @param maximum_well_capacity          is the maximum well capacity in the model. If well capacity falls above this capacity, it is set to this maximum. Defaults to 3000 gallons per minute.
+#' @param irrigation_season_days         is the number of days in an irrigation season. Defaults to 70.
 #' @param first_year_of_GW               is the first year that the GW model exists. This may be different than the first year of simulation. Defaults to 1997.
 #' @param last_year_of_GW                is the last  year that the GW model exists. This may be different than the last  year of simulation. Defaults to 2007.
 #' @param irrigation_season_days         Number of days in an irrigation season. Defaults to 70.
 #' @return                               returns the output table.
 #' @export
-annual_model_no_CREP = function (soil_weather_file = "./input_files/Well_SoilType_WeatherStation.csv",
-                              well_capacity_files = "./Well Capacity",
-                              # econ_output_folder = "./Econ_output/results_with_subsidy/annual_results/",
-                              econ_output_folder = "./Econ_output/results with CREP/annual_results/",
-                              well_capacity_file_year = "./KS_DSSAT_output.csv",
-                              CREP_wells_data = "./CREP_wells.csv",
-                              first_year_of_simulation = 2000,
-                              default_well_capacity_col_name = "Well_Capacity(gpm)",
-                              missing_soil_types = "KS00000007",
-                              minimum_well_capacity = 0,
-                              maximum_well_capacity = 1000,
-                              first_year_of_GW = 1997,
-                              last_year_of_GW = 2007,
-                              irrigation_season_days = 70)
+annual_model_no_CREP = function(soil_weather_file = "./input_files/Well_SoilType_WeatherStation.csv",
+                               well_capacity_files = "./Well Capacity",
+                               econ_output_folder = "./Econ_output/results_with_CREP/annual_results",
+                               well_capacity_file_year = "./KS_DSSAT_output.csv",
+                               look_up_table  = "lookup_table_all_years_2.rds",
+                               CREP_wells_data = "./CREP_wells.csv",
+                               first_year_of_simulation = 2000,
+                               default_well_capacity_col_name = "Well_Capacity(gpm)",
+                               missing_soil_types = "KS00000007",
+                               minimum_well_capacity = 0,
+                               maximum_well_capacity = 800,
+                               well_capacity_intervals = 10,
+                               first_year_of_GW = 1997,
+                               last_year_of_GW = 2007,
+                               irrigation_season_days = 70
+)
 {
+  library(data.table)
   soil_type = fread(soil_weather_file)
   soil_type[, `:=`(Soil_Type, gsub("KSFC00000", "KS0000000",
                                    Soil_Type))]
@@ -82,6 +87,7 @@ annual_model_no_CREP = function (soil_weather_file = "./input_files/Well_SoilTyp
   # setkey(CREP_wells, V1)
   # setkey(well_capacity_data, Well_ID)
   # well_capacity_data = CREP_wells[well_capacity_data]
+  # well_capacity_data_NA_wells = well_capacity_data[complete.cases(id)]
   # well_capacity_data = well_capacity_data[is.na(id)]
   # well_capacity_data[, `:=`(id, NULL)]
   # setnames(well_capacity_data, old = "V1", new = "Well_ID")
@@ -94,7 +100,10 @@ annual_model_no_CREP = function (soil_weather_file = "./input_files/Well_SoilTyp
 
   well_capacity_data[, `:=`(Well_capacity, ifelse(Well_capacity <= minimum_well_capacity, minimum_well_capacity, Well_capacity))]
   well_capacity_data[, `:=`(Well_capacity, ifelse(Well_capacity >= maximum_well_capacity, maximum_well_capacity, Well_capacity))]
-  lookup_table_all_years_2 = readRDS("lookup_table_all_years_2.rds")
+  well_capacity_data[, Well_capacity := floor(Well_capacity/well_capacity_intervals)*well_capacity_intervals]
+  well_capacity_data[Well_capacity !=0, Well_capacity := Well_capacity + 1]
+
+  lookup_table_all_years_2 = readRDS(look_up_table)
   filenames = list.files(path = well_capacity_files, pattern = "*.csv",
                          full.names = TRUE)
   ldf <- lapply(filenames, fread, fill = T)
@@ -126,15 +135,15 @@ annual_model_no_CREP = function (soil_weather_file = "./input_files/Well_SoilTyp
                                              output_rate_acin_day)]
   econ_output[, `:=`(row, 1:.N)]
   econ_output[, year := year_2]
-  econ_output_in = fread("./Econ_output/KS_DSSAT_output.csv")
-  econ_output_in[, year := 0]
-  econ_output = rbind(econ_output_in, econ_output)
+  # econ_output_in = fread("./Econ_output/KS_DSSAT_output.csv")
+  # econ_output_in[, year := 0]
+  # econ_output = rbind(econ_output_in, econ_output)
   econ_output[is.na(output_rate_acin_day), `:=`(output_rate_acin_day,
                                                 0)]
   well_capacity_data = lookup_table_all_years_2[, .(Well_ID,
                                                     output_rate_acin_day)]
-  write.csv(econ_output, paste0(econ_output_folder, "Econ_output_", year_2,
-                                ".csv"), row.names = FALSE)
+
+  write.csv(econ_output, paste0(econ_output_folder, "Econ_output_", year_2, ".csv"), row.names = FALSE)
   write.csv(well_capacity_data, well_capacity_file_year, row.names = FALSE)
 }
 
--- R/gen_output_CREP.R
+++ R/gen_output_CREP.R
@@ -2,12 +2,14 @@
 #' @param well_capacity_for_econ         is the file name that all the output well capacities are written into. Defaults to "C:/Users/manirad/Downloads/test/Econ_output/well_capacity.csv".
 #' @param first_year_of_simulation       is the first year that the hydro-economic simulation starts. Defaults to 2000.
 #' @param default_well_capacity_col_name is the name of the well capacity column generated from the MODFLOW model. Defaults to 'Well_Capacity(gpm)' as this is the original column name we started with.
+#' @param sim_id_or_well_num             is the number id for the simulation or the number of retired wells.
 #' @return                               returns the output table.
 #' @export
 gen_output_CREP = function (well_capacity_files = "./Well Capacity",
           well_capacity_for_econ = "./Econ_output/well_capacity.csv",
           first_year_of_simulation = 2000,
-          default_well_capacity_col_name = "Well_Capacity(gpm)")
+          default_well_capacity_col_name = "Well_Capacity(gpm)",
+          sim_id_or_well_num = 1)
 {
   filenames = list.files(path = well_capacity_files, pattern = "*.csv",
                          full.names = TRUE)
@@ -22,8 +24,6 @@ gen_output_CREP = function (well_capacity_files = "./Well Capacity",
   ldf[, `:=`(file_name, substr(file_name, nchar(file_name) -
                                  16, nchar(file_name) - 13))]
   ldf[, `:=`(file_name, as.integer(file_name))]
-  # write.table(ldf, sep = ",", file = well_capacity_for_econ,
-  #             row.names = F, col.names = !file.exists(well_capacity_for_econ),
-  #             append = T)
+  ldf[, `:=`(simulation_id, sim_id_or_well_num)]
   write.csv(ldf, well_capacity_for_econ, row.names = F)
 }
