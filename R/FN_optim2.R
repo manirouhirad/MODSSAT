6e294180ab8fd85ec1aade9f7fc3cc65e5e2c053
Mani Rouhi Rad <mani.rr2003@gmail.com>
2024-04-10 22:47:43 -0500

    added FN_optim2.

--- R/FN_optim2.R
+++ R/FN_optim2.R
@@ -11,7 +11,7 @@ FN_optim2 = function(jj = 1) {
   library(data.table)
   foo_irr_3 <- well_capacity_data[well_capacity_data[[9]] == jj, ]
   foo_irr_3 = as.data.table(foo_irr_3)
-  foo_irr_3 = class(foo_irr_3)
+  foo_irr_3[, ifreq := 15.9]
   # foo_irr_3[ifreq > 15.9, ifreq := 15.9]
   # foo_irr_3[ifreq > KS_DSSAT[, max(IFREQ)], ifreq := KS_DSSAT[, max(IFREQ)]]
   # foo_irr_3_0  = foo_irr_3[ifreq == 0]
