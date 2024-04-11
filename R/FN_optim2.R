74233ad6d99b3d09f1349132ab8c17be91bf58ca
Mani Rouhi Rad <mani.rr2003@gmail.com>
2024-04-10 22:17:48 -0500

    added FN_optim2.

--- R/FN_optim2.R
+++ R/FN_optim2.R
@@ -22,7 +22,7 @@ FN_optim2 <- function(jj = 1) {
   foo_irr_3_N0$id <- seq_len(nrow(foo_irr_3_N0))
   
   foo = data.table(foo_irr_3_N0)
-  foo[rep(1:.N, (diff+1))]
+  # foo[rep(1:.N, (diff+1))]
   selected_rows <- foo[rep(seq_len(nrow(foo)), foo$diff + 1), ]
   
   
