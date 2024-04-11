69672260723c8992eb30d052442238f87ebfd3bb
Mani Rouhi Rad <mani.rr2003@gmail.com>
2024-04-10 22:56:28 -0500

    added FN_optim2.

--- R/FN_optim2.R
+++ R/FN_optim2.R
@@ -5,12 +5,7 @@
 
 
 FN_optim2 = function(jj = 1) {
-  # foo_irr_3 = well_capacity_data[Well_ID_grp == jj]
-  # well_capacity_data = data.table(well_capacity_data)
-  # foo_irr_3 <- well_capacity_data[, .SD[.SD[[9]] == jj]]
-  library(data.table)
   foo_irr_3 <- well_capacity_data[well_capacity_data[[9]] == jj, ]
-  foo_irr_3 = as.data.table(foo_irr_3)
   max_IFREQ <- max(KS_DSSAT[["IFREQ"]], na.rm = TRUE)
   foo_irr_3[foo_irr_3$ifreq > max_IFREQ, "ifreq"] <- max_IFREQ
   foo_irr_3_0  = foo_irr_3[foo_irr_3$ifreq == 0,]
@@ -22,31 +17,40 @@ FN_optim2 = function(jj = 1) {
                         by.x = c("Soil_Type", "weather_station", "CR", "ifreq"),
                         by.y = c("SOIL_ID", "WSTA", "CR", "IFREQ"), allow.cartesian = T)
   }, error=function(e){})
-  # 
-  foo_irr_3_N0[, diff := (KS_DSSAT[, max(IFREQ)] - ifreq) * (1/IFREQ_interpolate)]
-  foo_irr_3_N0[, id := 1:.N]
-  # 
-  # tryCatch({
-  #   foo_irr_3_N0 = foo_irr_3_N0[rep(1:.N, (diff+1))]
-  #   foo_irr_3_N0[, foo := 1:.N, by=c("id")]
-  #   foo_irr_3_N0[, foo := foo - 1]
-  #   foo_irr_3_N0[, ifreq := ifreq + foo * IFREQ_interpolate]
-  #   foo_irr_3_N0[, ifreq := as.numeric(as.character(ifreq))]
-  # }, error=function(e){})
-  # 
-  # foo_irr_3_N0 = merge(foo_irr_3_N0, KS_DSSAT,
-  #                      by.x = c("Soil_Type", "weather_station", "CR", "ifreq"),
-  #                      by.y = c("SOIL_ID", "WSTA", "CR", "IFREQ"), allow.cartesian = T)
-  # foo_irr_3_N0[, c("id", "diff", "foo") := NULL]
-  # 
-  # 
-  # foo_irr_3 = rbind(foo_irr_3_N0, foo_irr_3_0)
-  # # foo_irr_3[, ifreq_denom := ifreq/.2]
-  # # foo_irr_3 = foo_irr_3[ifreq_denom %in% 0:200]
-  # 
-  # foo_irr_3[ifreq == KS_DSSAT[, max(IFREQ)], profit := profit/10]
-  # 
-  # rm(foo_irr_3_N0, foo_irr_3_0)
+  
+  foo_irr_3_N0$diff <- (max_IFREQ - foo_irr_3_N0$ifreq) * (1/IFREQ_interpolate)
+  foo_irr_3_N0$id <- seq_len(nrow(foo_irr_3_N0))
+  
+  foo = data.table(foo_irr_3_N0)
+  foo[rep(1:.N, (diff+1))]
+  selected_rows <- foo[rep(seq_len(nrow(foo)), foo$diff + 1), ]
+  
+  
+  
+  
+  tryCatch({
+    foo_irr_3_N0 = foo_irr_3_N0[rep(seq_len(nrow(foo_irr_3_N0)), foo_irr_3_N0$diff+1),]
+    foo_irr_3_N0$foo <- ave(rep(1, nrow(foo)), foo$id, FUN = seq_along)
+    
+    # library(dplyr)
+    # foo_irr_3_N0 <- foo_irr_3_N0 %>% 
+    #   group_by(id) %>% 
+    #   mutate(foo = row_number())
+    
+    foo_irr_3_N0$foo   = foo_irr_3_N0$foo - 1
+    foo_irr_3_N0$ifreq = foo_irr_3_N0$ifreq + (foo_irr_3_N0$foo * IFREQ_interpolate)
+    foo_irr_3_N0$ifreq = as.numeric(as.character(foo_irr_3_N0$ifreq))
+  }, error=function(e){})
+  
+  
+  foo_irr_3_N0 = merge(foo_irr_3_N0, KS_DSSAT,
+                       by.x = c("Soil_Type", "weather_station", "CR", "ifreq"),
+                       by.y = c("SOIL_ID", "WSTA", "CR", "IFREQ"), allow.cartesian = T)
+  foo_irr_3_N0[, c("id", "diff", "foo")] = NULL
+  foo_irr_3 = rbind(foo_irr_3_N0, foo_irr_3_0)
+  foo_irr_3$profit[foo_irr_3$ifreq == max_IFREQ] <- foo_irr_3$profit[foo_irr_3$ifreq == max_IFREQ] / 10
+  rm(foo_irr_3_N0, foo_irr_3_0)
+  
   return(foo_irr_3_N0)
   # 
   # foo_irr_3[, ifreq_cap := min(ifreq), by=c("Well_capacity", "SDAT", "tot_acres", "CR", "PAW")]
